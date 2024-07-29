d79808895a2b4eca7796d7c82f97125a03c49c0f
dangquocthinhformdasouth <thinhd601@gmail.com>
2024-07-29 18:08:18 +0700

    push code

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('.owl-carousel1').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+});
+$(document).ready(function() {
+    $('.owl-carousel2').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+});
